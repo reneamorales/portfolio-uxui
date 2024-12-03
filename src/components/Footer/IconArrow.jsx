2e39338c5e4a3daf942079c58007890371e5b0fa
René Alejandro Morales <renemoralesalejandro@gmail.com>
2024-12-03 18:20:25 -0300

    Agrego componente Footer empleando el uso de BEM

---
+++ src/components/Footer/IconArrow.jsx
@@ -0,0 +1,15 @@
+// IconArrow.js
+const IconArrow = () => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      className="icon"
+    >
+      <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5L21 12M21 12l-7.5 7.5M21 12H3" />
+    </svg>
+  );
+  
+  export default IconArrow;
