9349e7ea5719e7f98f9d16322e0787f807ed1fbd
René Alejandro Morales <renemoralesalejandro@gmail.com>
2024-12-27 00:22:26 -0300

    Agregar botón componentizado con lógica para la descarga del PDF

---
+++ src/components/Hero/DownloadCv.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const DownloadCv= () => {
+
+    const handleDownload = () => {
+
+        /*console.log("Evento de clic detectado");*/
+        const link = document.createElement('a');
+        link.href = '/René Alejandro Morales - QC Manual & Automation v1.2.pdf';
+        link.download = 'René Alejandro Morales - QC Manual & Automation.pdf';
+        link.click();
+    }
+    return (
+        <button onClick={handleDownload} className="btn btn-cv" >Descargar Cv</button>
+    );
+}
+
