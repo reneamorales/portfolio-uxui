9bbe74800424af89538aca87e77c96fc9eb9b319
René Alejandro Morales <renemoralesalejandro@gmail.com>
2025-04-16 00:08:19 -0300

    Agrega validación con PropTypes a ColorPalette: se permite texto y JSX en 'description'

--- src/ProjectOverview/DesignSystem/ColorPalette/ColorPalette.jsx
+++ src/ProjectOverview/DesignSystem/ColorPalette/ColorPalette.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import './ColorPalette.css';
-import { ItemContent } from './ItemContent';
+import PropTypes from 'prop-types';
+import { ItemContent, ItemParagraph } from './ItemContent';
 import { Color } from './Color'
-import { ItemParagraph } from "./ItemContent";
-export const ColorPalette = ({ titleSection }) => {
+
+export const ColorPalette = () => {
   return (
     <div className="design__process-container">
-      <p className="design__process-title">{titleSection}</p>
+      <p className="design__process-title">{ItemParagraph.title}</p>
       <div className="design__process-description">{ItemParagraph.description}</div>
       <div className="color__palette-inner">
         {ItemContent.map((item, index) => (
@@ -16,10 +17,10 @@ export const ColorPalette = ({ titleSection }) => {
               {item.colors.map((color, colorIndex) => (
                 <Color
                   key={colorIndex}
-                  name={color.name} // Nombre del color
-                  hex={color.hex} // Código del color o gradiente
+                  name={color.name} 
+                  hex={color.hex} 
                   direction={color.direction}
-                  textClass={color.textClass} // Clase de texto
+                  textClass={color.textClass} 
                 />
               ))}
             </div>
@@ -29,5 +30,17 @@ export const ColorPalette = ({ titleSection }) => {
     </div>
   );
 };
+
+ColorPalette.propTypes = {
+    content: PropTypes.shape({
+        title: PropTypes.string,
+        item: PropTypes.arrayOf(
+            PropTypes.shape({
+                imageSrc: PropTypes.string.isRequired,
+                description: PropTypes.node.isRequired // <- Permite texto, JSX, elementos React
+            })
+        )
+    })
+}
 
 export default ColorPalette;
