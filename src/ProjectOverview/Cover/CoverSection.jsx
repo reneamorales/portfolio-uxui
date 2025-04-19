dd954000f81838a1c6e1defcaecc20f68ba0dda9
René Alejandro Morales <renemoralesalejandro@gmail.com>
2025-04-18 23:59:29 -0300

    Agregar cambios en estructura del formato render, reset para soportar efecto gsap durante scroll

--- src/ProjectOverview/Cover/CoverSection.jsx
+++ src/ProjectOverview/Cover/CoverSection.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './cover-section.css';
 
-export const Cover = ()=>{
+export const Cover = () => {
     return (
         <div className="cover">
             <div className="cover__inner">
-                <div className="cover__title">Brand</div>
-                <div className="cover__text">This is a cover from Project</div>
+                <div className="cover__title">
+                    <span className="symbol">{'{'}</span>
+                    <span className="brand-name">Cranbelo</span>
+                    <span className="symbol">{'}'}</span>
+                </div>
+
+                <div className="cover__text">Rediseño Dinámico y Funcional del Sitio Web Cranbelo</div>
 
             </div>
         </div>
