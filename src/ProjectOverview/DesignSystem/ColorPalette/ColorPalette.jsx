37865365d8605b739bcca1816796de81b064900f
René Alejandro Morales <renemoralesalejandro@gmail.com>
2025-05-08 03:25:03 -0300

    refactor: aplicar formato consistente y mejoras menores en ColorPalette.jsx
    - Estilo de comillas unificado a dobles en imports.
    - Agregado salto de línea para mejorar la legibilidad de JSX.
    - Formato consistente aplicado en props del componente Color.
    - Se corrigió la indentación en PropTypes.
    - Se agregó una coma faltante en PropTypes.

--- src/ProjectOverview/DesignSystem/ColorPalette/ColorPalette.jsx
+++ src/ProjectOverview/DesignSystem/ColorPalette/ColorPalette.jsx
@@ -1,14 +1,16 @@
 import React from "react";
-import './ColorPalette.css';
-import PropTypes from 'prop-types';
-import { ItemContent, ItemParagraph } from './ItemContent';
-import { Color } from './Color'
+import "./ColorPalette.css";
+import PropTypes from "prop-types";
+import { ItemContent, ItemParagraph } from "./ItemContent";
+import { Color } from "./Color";
 
 export const ColorPalette = () => {
   return (
     <div className="design__process-container">
       <p className="design__process-title">{ItemParagraph.title}</p>
-      <div className="design__process-description">{ItemParagraph.description}</div>
+      <div className="design__process-description">
+        {ItemParagraph.description}
+      </div>
       <div className="color__palette-inner">
         {ItemContent.map((item, index) => (
           <div key={index} className="color__palette-item-group">
@@ -17,10 +19,10 @@ export const ColorPalette = () => {
               {item.colors.map((color, colorIndex) => (
                 <Color
                   key={colorIndex}
-                  name={color.name} 
-                  hex={color.hex} 
+                  name={color.name}
+                  hex={color.hex}
                   direction={color.direction}
-                  textClass={color.textClass} 
+                  textClass={color.textClass}
                 />
               ))}
             </div>
@@ -32,15 +34,15 @@ export const ColorPalette = () => {
 };
 
 ColorPalette.propTypes = {
-    content: PropTypes.shape({
-        title: PropTypes.string,
-        item: PropTypes.arrayOf(
-            PropTypes.shape({
-                imageSrc: PropTypes.string.isRequired,
-                description: PropTypes.node.isRequired // <- Permite texto, JSX, elementos React
-            })
-        )
-    })
-}
+  content: PropTypes.shape({
+    title: PropTypes.string,
+    item: PropTypes.arrayOf(
+      PropTypes.shape({
+        imageSrc: PropTypes.string.isRequired,
+        description: PropTypes.node.isRequired, // <- Permite texto, JSX, elementos React
+      })
+    ),
+  }),
+};
 
 export default ColorPalette;
