bdda239b63b7b101ad61cc9189dca75cd9a48052
René Alejandro Morales <renemoralesalejandro@gmail.com>
2025-05-21 23:47:08 -0300

    chore: mover contenido de proyectos a componente independiente

---
+++ src/components/Projects/projectData.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+export const projectData = [
+        {
+            title: 'Titulo del Proyecto 1',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id urna mollis, viverra risus id, tempus tortor. Sed tempus diam eget ex placerat gravida. Etiam eu dolor nisi. Donec luctus laoreet urna et convallis. Sed et sapien sit amet neque tempus semper. Nulla consequat magna.',
+            videoSrc: '/resources/video/4549682-hd_1920_1080_30fps.mp4', 
+            // Esta es la ruta que debes asegurarte que funcione
+            webLink: 'proyecto1-detalle.html',
+            projectLink: 'proyecto1-detalle.html',
+            caption: 'Vista previa del proyecto 1'
+        },
+        {
+            title: 'Titulo del Proyecto 2',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id urna mollis, viverra risus id, tempus tortor. Sed tempus diam eget ex placerat gravida. Etiam eu dolor nisi. Donec luctus laoreet urna et convallis. Sed et sapien sit amet neque tempus semper. Nulla consequat magna.',
+            videoSrc: '/resources/video/4549682-hd_1920_1080_30fps.mp4', 
+            webLink: 'proyecto2-detalle.html',
+            projectLink: 'proyecto2-detalle.html',
+            caption: 'Vista previa del proyecto 2'
+        },
+        {
+            title: 'Titulo del Proyecto 3',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id urna mollis, viverra risus id, tempus tortor. Sed tempus diam eget ex placerat gravida. Etiam eu dolor nisi. Donec luctus laoreet urna et convallis. Sed et sapien sit amet neque tempus semper. Nulla consequat magna.',
+            videoSrc: '/resources/video/4549682-hd_1920_1080_30fps.mp4', 
+            // Esta es la ruta que debes asegurarte que funcione
+            webLink: 'proyecto1-detalle.html',
+            projectLink: 'proyecto1-detalle.html',
+            caption: 'Vista previa del proyecto 1'
+        },
+        {
+            title: 'Titulo del Proyecto 4',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id urna mollis, viverra risus id, tempus tortor. Sed tempus diam eget ex placerat gravida. Etiam eu dolor nisi. Donec luctus laoreet urna et convallis. Sed et sapien sit amet neque tempus semper. Nulla consequat magna.',
+            videoSrc: '/resources/video/4549682-hd_1920_1080_30fps.mp4', 
+            // Esta es la ruta que debes asegurarte que funcione
+            webLink: 'proyecto1-detalle.html',
+            projectLink: 'proyecto1-detalle.html',
+            caption: 'Vista previa del proyecto 1'
+        }
+    ];
+
