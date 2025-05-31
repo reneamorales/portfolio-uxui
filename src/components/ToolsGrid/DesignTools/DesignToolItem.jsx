199bf105c9eb6d25a2937dc83141f92f097ea615
René Alejandro Morales <renemoralesalejandro@gmail.com>
2025-05-30 22:03:26 -0300

    agrego componente DesignTools modular dentro del fichero de componente que lo renderixa

---
+++ src/components/ToolsGrid/DesignTools/DesignToolItem.jsx
@@ -0,0 +1,38 @@
+import ejemploImage1 from '../../../img/brands/logo-figma.png';
+import ejemploImage2 from '../../../img/brands/logo-notion.png';
+import ejemploImage3 from '../../../img/brands/logo-figjam.png';
+import ejemploImage4 from '../../../img/brands/spline-logo.jpeg';
+import ejemploImage5 from '../../../img/brands/logo-chatGPT.png';
+
+export const designTools = [
+    {
+        name: "Figma",
+        category: "Diseño UX/UI",
+        image: ejemploImage1,
+        alt: "Logo de Figma"
+    },
+    {
+        name: "FigJam",
+        category: "Ideación ",
+        image: ejemploImage3,
+        alt: "Logo de Figjam"
+    },{
+        name: "Notion",
+        category: "Planificación",
+        image: ejemploImage2,
+        alt: "Logo de Notion"
+    },
+    {
+        name: "Spline",
+        category: "Diseño 3D",
+        image: ejemploImage4,
+        alt: "Logo de Spline"
+    },
+    {
+        name: "ChatGPT",
+        category: "Asistencia",
+        image: ejemploImage5,
+        alt: "Logo de ChatGPT"
+    }
+];
+
---
+++ src/components/ToolsGrid/DesignTools/DesignTools.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { ToolsGrid } from "../ToolsGrid";
+import "./design-tools.css";
+import { designTools } from "./DesignToolItem";
+
+export const DesignTools = () => {
+  const renderDesignTool = (tool) => (
+    <div key={tool.name} className="tool-container design-tool-container">
+      <img src={tool.image} alt={tool.alt} className="tool-logo" />
+      <div className="tool-text">
+        <p className="tool-name">{tool.name}</p>
+        <p className="tool-category">{tool.category}</p>
+      </div>
+    </div>
+  );
+
+  return (
+    <ToolsGrid
+      containerClassName="design-tools-section"
+      title="Herramientas que potencian mi proceso"
+      items={designTools}
+      gridClassName="items-grid-design"
+      renderItem={renderDesignTool}
+    />
+  );
+};
