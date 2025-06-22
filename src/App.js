a63e0a2fc1c21f7430629c615a8502be9aa437e0
René Alejandro Morales <renemoralesalejandro@gmail.com>
2025-06-22 06:18:38 -0300

    declarando las importaciones de los componentes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-
 import './App.css';
 
+// Componentes de navegación y principales
 import {Navbar} from './components/Navbar/Navbar';
 import {Hero} from './components/Hero/Hero';
 import {Projects} from './components/Projects/Projects';
 import {Principles} from './components/Principles/Principles';
-import {Tools} from './components/Tools/Tools';
+import {DesignTools} from './components/ToolsGrid/DesignTools/DesignTools';
+import {CodeTools} from './components/ToolsGrid/CodeTools/CodeTools';
+
+// Componentes de ProjectOverview 
+import {Cover} from './ProjectOverview/Cover/CoverSection';
+import {CoreDetails} from './ProjectOverview/CoreSections/CoreDetails';
+import {Grid} from './ProjectOverview/ProcessDesign/GridWireframes/Grid';
+import {ColorPalette} from './ProjectOverview/DesignSystem/ColorPalette/ColorPalette';
+import {TipographySection} from './ProjectOverview/DesignSystem/TipographySection/TipographySection';
+import {AlternatingProcessGrid} from './ProjectOverview/ProcessDesign/AlternatingProcessGrid/AlternatingProcessGrid';
+import {RedirectionSection} from './ProjectOverview/Navigation/Redirection/RedirectionSection';
+import {Footer} from './components/Footer/Footer';
+
+// Contenido y datos
+import {initialSection, designSolutionsContent, finalSection} from './ProjectOverview/CoreSections/CoreContent';
+import {wireframes, hiPiPrototype, assetsUi, keyDisplay} from './ProjectOverview/ProcessDesign/GridWireframes/ItemContent';
+import {spaces, interaction} from './ProjectOverview/ProcessDesign/AlternatingProcessGrid/AlternatingProcessGridContent';
 
 function App() {
 
