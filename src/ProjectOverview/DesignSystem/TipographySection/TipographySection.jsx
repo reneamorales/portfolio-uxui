42a4561e32d4a2a43b68d3c5e27302e6dfc9a311
René Alejandro Morales <renemoralesalejandro@gmail.com>
2025-06-30 23:45:21 -0300

    refactor: aplica AnimatedSectionTitle y DesignProcessDescription en TipographySection
    - Reemplaza título y descripción estáticos por componentes reutilizables para unificar estilo y comportamiento.
    - Mejora mantenibilidad y consistencia visual con el resto del sistema.
    - Mantiene estructura de tipografías con ItemFontContent.

--- src/ProjectOverview/DesignSystem/TipographySection/TipographySection.jsx
+++ src/ProjectOverview/DesignSystem/TipographySection/TipographySection.jsx
@@ -1,35 +1,37 @@
-import React from 'react'
-import './TipographySection.css'
-import PropTypes from 'prop-types'
-import { ItemFont } from './ItemFont'
-import { ItemFontContent } from './ItemFontContent'
-import { ItemParagraph } from './ItemFontContent';
+import React from "react";
+import "./TipographySection.css";
+import { AnimatedSectionTitle } from "../../../components/AnimatedSectionTitle/AnimatedSectionTitle";
+import DesignProcessDescription from "../../DesignProcessDescription/DesignProcessDescription";
+import PropTypes from "prop-types";
+import { ItemFont } from "./ItemFont";
+import { ItemFontContent } from "./ItemFontContent";
+import { ItemParagraph } from "./ItemFontContent";
 
 export const TipographySection = () => {
-    return (
-        <div
-            className='design__process-container'>
-            <h2 className='design__process-title'>{ItemParagraph.title}</h2>
-            <p className='design__process-description'>{ItemParagraph.description}</p>
-            <div className='tipography__section-inner'>
-                {ItemFontContent.map((font, index) => (
-                    <ItemFont
-                        key={index}
-                        type={font.type}
-                        name={font.name}
-                        abc={font.abc}
-                        numbers={font.numbers}
-                    />
-                ))}
-            </div>
-        </div>
-    )
-    
-}
+  return (
+    <div className="design__process-container">
+      <AnimatedSectionTitle className="section__subtitle">
+        {ItemParagraph.title}
+      </AnimatedSectionTitle>
+      <DesignProcessDescription>{ItemParagraph.description}</DesignProcessDescription>
+      <div className="tipography__section-inner">
+        {ItemFontContent.map((font, index) => (
+          <ItemFont
+            key={index}
+            type={font.type}
+            name={font.name}
+            abc={font.abc}
+            numbers={font.numbers}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 TipographySection.propTypes = {
-        paragraph: PropTypes.shape({
-            title: PropTypes.node.isRequired,
-            description: PropTypes.node.isRequired
-        })
-    }
+  paragraph: PropTypes.shape({
+    title: PropTypes.node.isRequired,
+    description: PropTypes.node.isRequired,
+  }),
+};
