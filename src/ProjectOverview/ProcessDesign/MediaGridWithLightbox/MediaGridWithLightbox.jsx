af8034283257ad852ffac0eef2ac395ee3f4e040
René Alejandro Morales <renemoralesalejandro@gmail.com>
2025-07-08 22:20:03 -0300

    feat: agregar estructura del componente MediaGridWithLightbox (pendiente integración de lightbox)

---
+++ src/ProjectOverview/ProcessDesign/MediaGridWithLightbox/MediaGridWithLightbox.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import { Item } from "../GridWireframes/Item"; 
+import Lightbox from "";
+
+import { AnimatedSectionTitle } from "../../../components/AnimatedSectionTitle/AnimatedSectionTitle";
+
+export const MediaGridWithLightbox = ({ content }) => {
+  const [isLightboxOpen, setIsLightboxOpen] = useState(false);
+  const [currentImageSrc, setCurrentImageSrc] = useState("");
+
+  const openLightbox = (imageSrc) => {
+    setCurrentImageSrc(imageSrc);
+    setIsLightboxOpen(true);
+  };
+
+  const closeLightbox = () => {
+    setIsLightboxOpen(false);
+    setCurrentImageSrc("");
+  };
+
+  return (
+    <div className="grid-container"> 
+      <div className="grid-item">
+       <AnimatedSectionTitle className="section__subtitle">
+          {content.title}
+        </AnimatedSectionTitle>
+
+        <div className="grid"> 
+          {content.item.map((item, index) => (
+            <Item
+              key={index}
+              imageSrc={item.imageSrc}
+              
+              onImageClick={() => openLightbox(item.imageSrc)}
+            />
+          ))}
+        </div>
+
+        
+      </div>
+
+      {isLightboxOpen && (
+        <Lightbox imageSrc={currentImageSrc} onClose={closeLightbox} />
+      )}
+    </div>
+  );
+};
