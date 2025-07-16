03f578fe7bb34b44da1d61b7228ad8e41f11d084
René Alejandro Morales <renemoralesalejandro@gmail.com>
2025-07-15 21:16:57 -0300

    agregar el camponente AppRouter

---
+++ src/AppRouter/AppRouter.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import Home from "../pages/Home";
+import About from "../pages/About";
+import ProjectsPage from "../pages/ProjectsPage";
+import ProjectDetail from "../pages/ProjectDetail";
+import Pagina404 from "../pages/Pagina404";
+import { Navbar } from "../components/Navbar/Navbar";
+import { Footer } from "../components/Footer/Footer";
+
+export const AppRouter = ({ children }) => {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" replace />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<ProjectsPage />} />
+        <Route path="/projects/:id" element={<ProjectDetail />} />
+        <Route path="*" element={<Navigate to="/404" />} />
+        <Route path="/404" element={<Pagina404 />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+};
