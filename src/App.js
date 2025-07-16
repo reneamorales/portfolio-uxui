0a3e10bd0147737b1c7aea57b095c55e39530288
René Alejandro Morales <renemoralesalejandro@gmail.com>
2025-07-15 21:19:46 -0300

    refactor(app): reemplaza estructura estática por enrutamiento con AppRouter

--- src/App.js
+++ src/App.js
@@ -1,38 +1,12 @@
 import './App.css';
 
-// Componentes de navegación y principales
-import {Navbar} from './components/Navbar/Navbar';
-import {Hero} from './components/Hero/Hero';
-import {Projects} from './components/Projects/Projects';
-import {Principles} from './components/Principles/Principles';
-import {DesignTools} from './components/ToolsGrid/DesignTools/DesignTools';
-import {CodeTools} from './components/ToolsGrid/CodeTools/CodeTools';
-
-// Componentes de ProjectOverview 
-import {Cover} from './ProjectOverview/Cover/CoverSection';
-import {CoreDetails} from './ProjectOverview/CoreSections/CoreDetails';
-import {Grid} from './ProjectOverview/ProcessDesign/GridWireframes/Grid';
-import {ColorPalette} from './ProjectOverview/DesignSystem/ColorPalette/ColorPalette';
-import {TipographySection} from './ProjectOverview/DesignSystem/TipographySection/TipographySection';
-import {AlternatingProcessGrid} from './ProjectOverview/ProcessDesign/AlternatingProcessGrid/AlternatingProcessGrid';
-import {RedirectionSection} from './ProjectOverview/Navigation/Redirection/RedirectionSection';
-import {Footer} from './components/Footer/Footer';
-
-// Contenido y datos
-import {initialSection, designSolutionsContent, finalSection} from './ProjectOverview/CoreSections/CoreContent';
-import {wireframes, hiPiPrototype, assetsUi, keyDisplay} from './ProjectOverview/ProcessDesign/GridWireframes/ItemContent';
-import {spaces, interaction} from './ProjectOverview/ProcessDesign/AlternatingProcessGrid/AlternatingProcessGridContent';
+import { AppRouter } from "./AppRouter/AppRouter";
 
 function App() {
-
   return (
-    <div className="App">
-      <Navbar />
-      <Hero/>
-      <Projects/>
-      <Principles/>
-      <Tools/>
-    </div>
+
+    <AppRouter />
+
   );
 }
 
